bb3e53169ae132db958abc2fb36e72e61d9d3deb
akkiie0300 <akash.vishwakarma@indianexpress.com>
2023-10-18 17:11:13 +0530

    first commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/home.css';
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <header>Home Page</header>
+        <a href='/' className='backbutton'>Back to Home</a>
+    </div>
+  )
+}
+
+export default Home
